891dab67df818bb1f616b8a28a2d57954500eee4
Яров Михаил <vbif.misha2012@yandex.ru>
2021-05-29 21:15:45 +0300

    pre similar products

--- src/components/SimilarProducts.vue
+++ src/components/SimilarProducts.vue
@@ -4,7 +4,16 @@
       Похожие товары:
     </div>
     <div class="slider">
-
+      <div class="image-handler arrow arrow-left"></div>
+      <ul class="elems">
+        <li class="image-handler product"></li>
+        <li class="image-handler product"></li>
+        <li class="image-handler product"></li>
+        <li class="image-handler product"></li>
+        <li class="image-handler product"></li>
+        <li class="image-handler product"></li>
+      </ul>
+      <div class="image-handler arrow arrow-right"></div>
     </div>
   </div>
 </template>
@@ -15,6 +24,39 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.similar-products {
+  & ._title {
+    font-family: Montserrat-regular, serif;
+    font-size: 24px;
+  }
 
+  & .slider {
+    margin-top: 15px;
+    width: 1200px;
+    height: 160px;
+    background-color: #A6A9FF;
+
+    //height: fit-content;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    text-align: center;
+
+    & .elems {
+      padding: 2em;
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+      justify-content: space-between;
+
+      & .product {
+        list-style: none;
+        width: 166px;
+        height: 158px;
+        background-color: #ADE2FF;
+      }
+    }
+  }
+}
 </style>
